8c9070005dd04b8b812c0cf14ff9fc79b0af8ea8
lucas mendes <lucasmndes.dev@gmail.com>
2025-09-07 10:58:13 -0300

    pequeno ajuste de cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: "https://codenotes-wheat.vercel.app",
+    origin: [
+      "https://codenotes-wheat.vercel.app",
+      "http://localhost:3000"
+    ],
+
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe())
